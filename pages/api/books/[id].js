7b24663fc168f92ae931e8fb58cd599f836498e8
Greg Wiley <gcwiley@gmail.com>
2020-07-05 10:17:04 -0400

    addtional code added

--- pages/api/books/[id].js
+++ pages/api/books/[id].js
@@ -1,33 +1,33 @@
-// import dbConnect from '../../../utils/dbConnect';
-// import Book from '../../../models/Book';
+import dbConnect from '../../../utils/dbConnect';
+import Book from '../../../models/Book';
 
-// export default async function handler(req, res) {
-//     const {
-//         query: { id },
-//         method,
-//     } = req
+export default async function handler(req, res) {
+    const {
+        query: { id },
+        method,
+    } = req
 
-//     await dbConnect()
+    await dbConnect()
 
-//     switch (method) {
-//         case 'GET':
-//             try {
-//                 const pet = await Book.findById(id)
-//                 if (!book) {
-//                     return res.status(400).json({ success: false })
-//                 }
-//                 res.status(200).json({ success: true, data: book })
-//             } catch (error) {
-//                 res.status(400).json({ success: false })
-//             }
-//             break
+    switch (method) {
+        case 'GET':
+            try {
+                const pet = await Book.findById(id)
+                if (!book) {
+                    return res.status(400).json({ success: false })
+                }
+                res.status(200).json({ success: true, data: book })
+            } catch (error) {
+                res.status(400).json({ success: false })
+            }
+            break
 
-//         case 'PUT':
-//             try {
-//                 const book = await Book.findByIdAndUpdate(id, res.body, {
-//                     new: true,
-//                     runValidators: true,
-//                 })
-//             }
-//     }
-// }
+        case 'PUT':
+            try {
+                const book = await Book.findByIdAndUpdate(id, res.body, {
+                    new: true,
+                    runValidators: true,
+                })
+            }
+    }
+}
--- pages/api/books/index.js
+++ pages/api/books/index.js
@@ -1,32 +1,32 @@
 import dbConnect from '../../../utils/dbConnect';
 import Book from '../../../models/Book';
 
-// export default async function handler(req, res) {
-//     const { method } = req
+export default async function handler(req, res) {
+    const { method } = req
     
-//     await dbConnect()
+    await dbConnect()
 
-//     switch (method) {
-//         case 'GET':
-//             try {
-//                 const books = await Book.find({})
-//                 res.status(200).json({ success: true, data: books })
-//             } catch (error) {
-//                 res.status(400).json({ success: false })
-//             }
-//             break
-//         case 'POST':
-//             try {
-//                 const book = await Book.create(
-//                     res.body
-//                 )
-//                 res.status(201).json({ success: true, data: book })
-//             } catch (error) {
-//                 res.status(400).json({ success: false })
-//             }
-//             break
-//         default:
-//             res.status(400).json({ success: false })
-//             break
-//     }
-// }
+    switch (method) {
+        case 'GET':
+            try {
+                const books = await Book.find({})
+                res.status(200).json({ success: true, data: books })
+            } catch (error) {
+                res.status(400).json({ success: false })
+            }
+            break
+        case 'POST':
+            try {
+                const book = await Book.create(
+                    res.body
+                )
+                res.status(201).json({ success: true, data: book })
+            } catch (error) {
+                res.status(400).json({ success: false })
+            }
+            break
+        default:
+            res.status(400).json({ success: false })
+            break
+    }
+}
